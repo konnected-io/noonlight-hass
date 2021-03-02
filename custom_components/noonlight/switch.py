5759dec9efc2d11ef0661dd6238fd9ed7aee72fe
0n3man <bkprath@gmail.com>
2021-03-02 00:46:29 +0000

    Modified switch to support V2 API with support for address, emergency service and alert cause

--- custom_components/noonlight/switch.py
+++ custom_components/noonlight/switch.py
@@ -73,15 +73,65 @@ class NoonlightSwitch(SwitchEntity):
         #   whether medical, fire, or police should be notified
         if self._alarm is None:
             try:
-                self._alarm = await self.noonlight.client.create_alarm(
-                    body={
+
+                # dispatch is update to V2 noonlight API, added change here to use address
+                # for location reported to noonlight and also include desired service and
+                # special interaction instructions
+                if "dispatch" in self.noonlight.config.get('api_endpoint'):
+
+                    # if you have a dev_token you can set it in the configuration so that you can
+                    # view the messages you're generating in the noonlight developer web site
+                    if 'dev_token' in self.noonlight.config.keys():
+                        self.noonlight.client.set_token( token = self.noonlight.config.get('dev_token') )
+                        _LOGGER.debug('noonlight client token set. '
+                               'token: %s', self.noonlight.config.get('dev_token'))
+
+                    # location and primary contact are required items in V2
+                    body_param={
+                        'location':{
+                            'address': {
+                                'line1': self.noonlight.config.get('line1'),
+                                'line2': self.noonlight.config.get('line2') or "N/A",
+                                'city': self.noonlight.config.get('city'),
+                                'state': self.noonlight.config.get('state'),
+                                'zip': self.noonlight.config.get('zip')
+                            }
+                        },
+                        'name': self.noonlight.config.get('name'),
+                        'phone': self.noonlight.config.get('phone'),
+                        'pin': self.noonlight.config.get('pin')
+                    }
+
+                    #try:
+                        #'instructions': { 'entry':self.noonlight.config.get('instructions') or "N/A"}
+                    #    body_param.update( {'instructions': { 'entry':self.hass.states.get('input_text.alarm_cause') or "N/A"}})
+                    #except TypeError:
+                    _LOGGER.debug('Alarm cause . '
+                               'reason: %s', self.hass.states.get('input_text.alarm_cause').state)
+                    body_param.update( {'instructions': { 'entry': 'Alarm cause is ' + self.hass.states.get('input_text.alarm_cause').state+
+                        "  "+self.noonlight.config.get('instructions')}})
+
+                    # if input_text item configured include the service element in the alert, default police
+                    if self.hass.states.is_state('input_text.noonlight_service','police'):
+                        body_param.update( {'services' : {'police' : True}} )
+                    elif self.hass.states.is_state('input_text.noonlight_service','fire'):
+                       body_param.update( {'services' : {'fire' : True}} )
+                    elif self.hass.states.is_state('input_text.noonlight_service','medical'):
+                        body_param.update( {'services' : {'medical' : True}} )
+                    else:
+                        body_param.update( {'services' : {'police' : True}} )
+
+                    self._alarm = await self.noonlight.client.create_alarm(body=body_param)
+                        
+                else:
+                    self._alarm = await self.noonlight.client.create_alarm(
+                        body={
                         'location.coordinates': {
                             'lat': self.noonlight.latitude,
                             'lng': self.noonlight.longitude,
                             'accuracy': 5
                         }
-                    }
-                )
+                        })
             except NoonlightClient.ClientError as client_error:
                 persistent_notification.create(
                     self.hass,
@@ -123,3 +173,5 @@ class NoonlightSwitch(SwitchEntity):
                 self._alarm = None
         if self._alarm is None:
             self._state = False
+
+        self.hass.states.async_set('input_text.alarm_cause','unknown')
